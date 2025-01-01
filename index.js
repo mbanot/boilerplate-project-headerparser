53a8fbd0072208364dc2f4e0a319fd8e863679b3
Tafara M. Mbano <tmmmbano@gmail.com>
2025-01-01 05:38:45 +0000

    Challenge Complete

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res){
+  res.json({
+    ipaddress: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+    language: navigator.language || navigator.userLanguage,
+    software: navigator.userAgent
+  })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
